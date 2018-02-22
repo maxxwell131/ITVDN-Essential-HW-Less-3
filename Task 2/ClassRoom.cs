fedc611f92526e773df8800e7a2d0b5feb53b36f
maxxwell131 <maxxwell131@yahoo.com>
2018-02-22 07:06:56 +0200

    Add class ClassRoom,Pupil,ExcelentPupil, GoodPupil, BadPul

---
+++ Task 2/ClassRoom.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class ClassRoom
+    {
+        public void Stady()
+        {
+            Console.WriteLine("ClassRoom stady");
+        }
+
+        public void Read()
+        {
+            Console.WriteLine("ClassRoom read");
+        }
+
+        public void Write()
+        {
+            Console.WriteLine("ClassRoom write");
+        }
+
+        public void Relax()
+        {
+            Console.WriteLine("ClassRoom relax");
+        }
+    }
+}
---
+++ Task 2/Pupil.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Pupil
+    {
+        public void Stady()
+        {
+            Console.WriteLine("Pupil stady");
+        }
+
+        public void Read()
+        {
+            Console.WriteLine("Pupil read");
+        }
+
+        public void Write()
+        {
+            Console.WriteLine("Pupil write");
+        }
+
+        public void Relax()
+        {
+            Console.WriteLine("Pupil relax");
+        }
+    }
+}
