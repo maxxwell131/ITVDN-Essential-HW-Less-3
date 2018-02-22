e50450b2666413974c7b3311b9e166bc8031f881
maxxwell131 <maxxwell131@yahoo.com>
2018-02-22 05:11:06 +0200

    Добавьте файлы проекта.

---
+++ ITVDN-Essential-HW-Less-3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITVDN_Essential_HW_Less_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
